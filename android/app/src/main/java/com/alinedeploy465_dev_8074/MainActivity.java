7cae0e5aabb90a9962da3a4a9fc67340a7268202
Crowdbotics <team@crowdbotics.com>
2020-07-23 19:12:32 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/alinedeploy465_dev_8074/MainActivity.java
@@ -0,0 +1,15 @@
+package com.alinedeploy465_dev_8074;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "alinedeploy465_dev_8074";
+    }
+}
